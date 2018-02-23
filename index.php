6c57caac560ffead78146ce6ed693a615f40289b
David Zambrano <david.zambrano@duwest.com>
2018-02-23 09:43:59 -0500

    Se agregael index para toda la app

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+header("Status: 301 Moved Permanently");
+header("Location: http://localhost/dufly/backoffice/");
+exit;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+
+</body>
+</html>
