aeb6027f922935bd89d29374a8c6f10b8bfd044f
David Zambrano - Ubuntu <david.zambrano@duwest.com.co>
2018-04-05 10:49:22 -0500

    se eliminan ajustes de la bd en el backoffice

--- backoffice/application/models/Login_model.php
+++ backoffice/application/models/Login_model.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
  if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Login_model extends CI_Model
 {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->database();
 	}
 	/*all functions here.*/
 	function autenticar($usu,$pass){
-		
+
 		return RestApi::call(
 			RestApiMethod::POST,
 			"auth/autenticar",
